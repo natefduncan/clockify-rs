606972019762efb02a991b20fd86313378de9401
Nate Duncan <natefduncan@gmail.com>
2022-03-01 16:21:51 -0600

    Add: Test tag in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-mod clockify; 
+mod clockify;
 mod endpoint; 
 mod tag;
 
-fn main() {
+use reqwest::blocking::Client; 
 
+use crate::endpoint::EndPoint; 
+use crate::clockify::Clockify;
+use crate::tag::Tag; 
+
+fn main() {
+    let c = Clockify {
+        api_key : "API_KEY".to_string(), 
+        workspace_id : "WORKSPACE_ID".to_string(),
+        client: Client::new(),
+    };
+    println!("{:?}", Tag::list(None, &c)); 
 }
+
