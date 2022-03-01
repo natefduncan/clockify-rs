75ee0a5d399e320e601c735b0894ec2fab4d95bd
Nate Duncan <natefduncan@gmail.com>
2022-03-01 16:05:23 -0600

    Add: client, api_key to Clockify

--- src/clockify.rs
+++ src/clockify.rs
@@ -1,5 +1,7 @@
 pub const BASE_URL : &str = "https:/api.clockify.me/api/v1";
 
 pub struct Clockify {
+    pub client : reqwest::blocking::Client, 
+    pub api_key : String,
     pub workspace_id : String
 }
