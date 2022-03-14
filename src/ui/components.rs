67acc9251a43d6d4083d20f886d96e99c5bafc09
Nate Duncan <natefduncan@gmail.com>
2022-03-14 10:02:34 -0500

    Add: Component trait and input box

--- src/ui/components.rs
+++ src/ui/components.rs
@@ -1,21 +1,76 @@
 use tui::{
-    widgets::{ListState, Widget, StatefulWidget}, 
+    backend::Backend, 
+    layout::{Constraint, Direction, Layout, Rect}, 
+    style::{Color, Modifier, Style}, 
+    symbols, 
+    text::{Span, Spans},
+    widgets::{Block, Borders, Cell, List, ListItem, Paragraph, Widget, StatefulWidget, ListState},
+    Frame
 }; 
+
+use std::fmt::Display;
+
+
+use crate::{
+    clockify::App,
+    api::EndPoint
+};
+use reqwest::blocking::Client;
+
+pub trait Component {
+   fn render<B: Backend>(&mut self, f: &mut Frame<B>, client: &Client, area: Rect);
+}
 
 #[derive(Debug, Clone)]
 pub struct InputBox {
-    pub title: String,
+    pub prompt: String,
     pub text: String, 
 }
 
+impl Component for InputBox {
+    fn render<B: Backend>(&mut self, f: &mut Frame<B>, client: &Client, area: Rect) {
+        let block = Block::default()
+            .title(self.prompt.clone())
+            .borders(Borders::ALL);
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .margin(5)
+            .constraints(
+                [
+                Constraint::Percentage(25), 
+                Constraint::Percentage(75)
+                ].as_ref()
+            ).split(area); 
+        f.render_widget(Paragraph::new(self.prompt.clone()), chunks[0]); 
+        f.render_widget(Paragraph::new(self.text.clone()), chunks[1]); 
+    }
+}
+
+impl From<&str> for InputBox {
+    fn from(s: &str) -> InputBox {
+        InputBox {
+            prompt: s.to_owned(), 
+            text: String::new()
+        }
+    }
+}
+
+impl From<String> for InputBox {
+    fn from(s: String) -> InputBox {
+        InputBox {
+            prompt: s, 
+            text: String::new()
+        }
+    }
+}
 
 #[derive(Debug, Clone)]
-pub struct StatefulList<T> {
+pub struct StatefulList<T: Display> {
     pub state: ListState, 
     pub items: Vec<T>
 }
 
-impl<T> StatefulList<T> {
+impl<T: Display> StatefulList<T> {
     pub fn with_items(items: Vec<T>) -> StatefulList<T> {
         StatefulList {
             state: ListState::default(), 
@@ -52,4 +107,17 @@ impl<T> StatefulList<T> {
     }
 }
 
+impl<T: Display> Component for StatefulList<T> {
+
+    fn render<B: Backend>(&mut self, f: &mut Frame<B>, client: &Client, area: Rect) {
+        let list_item : Vec<ListItem> = self.items.iter()
+           .map(|i| ListItem::new(vec![Spans::from(Span::raw(format!("{}", i)))]))
+           .collect();
+        let list_item = List::new(list_item)
+            .block(Block::default().borders(Borders::ALL))
+            .highlight_style(Style::default().add_modifier(Modifier::BOLD));
+        f.render_stateful_widget(list_item, area, &mut self.state)
+    }
+
+}
 
